feb1109e867f09aec1f332b8acc9b30a149608fd
NIKHIL-MEHTA1 <nikhilmehta8057709341@gmail.com>
2023-07-10 17:38:19 +0530

    first-commit

---
+++ app/components/TodoList.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {ITask} from "@/types/tasks";
+
+import Task from "./Task";
+
+interface TodoListProps {
+    tasks: ITask[]
+}
+
+const TodoList :React.FC<TodoListProps> = ({tasks}) => {
+  return (
+    <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>TASKS</th>
+       <th>ACTIONS</th>
+      </tr>
+    </thead>
+    <tbody>
+    {tasks.map(task=><Task key={task.id} task={task}/>)}
+    </tbody>
+  </table>
+</div>
+  )
+}
+
+export default TodoList
